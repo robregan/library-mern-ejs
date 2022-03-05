3c2a56ebaf10e95631cdd920cc9ce580123ed61b
rob <robert_regan86@yahoo.com>
2022-03-05 16:30:49 -0500

    added db connection

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
-const PORT = 3000
 const expressLayouts = require('express-ejs-layouts');
 const indexRouter = require('./routes/index')
+
+const connectDB = require('./config/database')
+const MongoStore = require("connect-mongo")
+const PORT = process.env.PORT || 3000
+require("dotenv").config({ path: "./config/.env" });
+
+//Body Parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
 app.set('view engine', 'ejs')
@@ -11,6 +19,17 @@ app.set('layout', 'layouts/layout')
 app.use(expressLayouts);
 app.use(express.static('public'))
 
+connectDB()
+
+// // Setup Sessions - stored in MongoDB
+// app.use(
+//     session({
+//       secret: "keyboard cat",
+//       resave: false,
+//       saveUninitialized: false,
+//       store: new MongoStore({ mongooseConnection: mongoose.connection }),
+//     })
+//   );
 
 
 app.use('/' , indexRouter)
